76454624ddceb6a8d221eeab2885e7e7035de681
joseluisposada <jlposada22@misena.edu.co>
2022-09-12 17:26:26 -0500

    correcion modales aun falta

--- js/ejer5ta.js
+++ js/ejer5ta.js
@@ -15,16 +15,6 @@ cermodal.addEventListener("click",()=>{
 })
 
 
-function pertrii(){ 
-  let pri=parseInt(document.getElementById("pritri").value);
-  let seg=parseInt(document.getElementById("segtri").value);
-  let tre=parseInt(document.getElementById("tertri").value);
-
-  var pertri = pri+seg+tre;
-  alert("El perimetro es = "+pertri)
-}
-
-
 /* area Triangulo */
 
 let btnmodalar=document.getElementById("aretri");
@@ -40,3 +30,101 @@ btnmodalar.addEventListener("click",()=>{
 cermodalar.addEventListener("click",()=>{
     venmodalar.style.display="none"; 
 })
+
+/* Perimetro Circulo */
+
+let btnmodalcir=document.getElementById("pericir");
+
+let venmodalcir=document.getElementById("ventanacir");
+
+let cermodalcir=document.querySelector(".cerrarmodalcir");
+
+btnmodalcir.addEventListener("click",()=>{
+    venmodalcir.style.display="block";
+})
+
+cermodalcir.addEventListener("click",()=>{
+    venmodalcir.style.display="none"; 
+})
+
+/* Area Circulo */
+
+let btnmodalcira=document.getElementById("arecir");
+
+let venmodalcira=document.getElementById("ventanacirar");
+
+let cermodalcira=document.querySelector(".cerrarmodalcirar");
+
+btnmodalcira.addEventListener("click",()=>{
+    venmodalcira.style.display="block";
+})
+
+cermodalcira.addEventListener("click",()=>{
+    venmodalcira.style.display="none"; 
+})
+
+
+/* Perimetro Cuadrado */
+
+let btnmodalcua=document.getElementById("pericua");
+
+let venmodalcua=document.getElementById("ventanacua");
+
+let cermodalcua=document.querySelector(".cerrarmodalcua");
+
+btnmodalcua.addEventListener("click",()=>{
+    venmodalcua.style.display="block";
+})
+
+cermodalcua.addEventListener("click",()=>{
+    venmodalcua.style.display="none"; 
+})
+
+/* Area Cuadrado */
+
+let btnmodalcuaa=document.getElementById("arecua");
+
+let venmodalcuaa=document.getElementById("ventanacuaa");
+
+let cermodalcuaa=document.querySelector(".cerrarmodalcuaa");
+
+btnmodalcuaa.addEventListener("click",()=>{
+    venmodalcuaa.style.display="block";
+})
+
+cermodalcuaa.addEventListener("click",()=>{
+    venmodalcuaa.style.display="none"; 
+})
+
+/* Perimetro Rectangulo */
+
+let btnmodalrec=document.getElementById("perirec");
+
+let venmodalrec=document.getElementById("ventanarec");
+
+let cermodalrec=document.querySelector(".cerrarmodalrec");
+
+btnmodalrec.addEventListener("click",()=>{
+    venmodalrec.style.display="block";
+})
+
+cermodalrec.addEventListener("click",()=>{
+    venmodalrec.style.display="none"; 
+})
+
+/* Area Rectangulo */
+
+let btnmodalreca=document.getElementById("arerec");
+
+let venmodalreca=document.getElementById("ventanareca");
+
+let cermodalreca=document.querySelector(".cerrarmodalreca");
+
+btnmodalreca.addEventListener("click",()=>{
+    venmodalreca.style.display="block";
+})
+
+cermodalreca.addEventListener("click",()=>{
+    venmodalreca.style.display="none"; 
+})
+
