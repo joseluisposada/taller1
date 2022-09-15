3eac36c7c226e6638b473f440d5737b347cf6e19
joseluisposada <jlposada@misena.edu.co>
2022-09-15 12:03:33 -0500

    se realizo validaciones

---
+++ js/validaciones.js
@@ -0,0 +1,110 @@
+//Esta es para nombre y apellidos ^[a-zA-ZÁ-ÿ\s]{1,40}$
+
+//rela para numdoc y telefono ^\d{7,10}$
+
+//regla correo ^[a-zA-Z0-9_+-.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$
+
+//Regla password ^(?=.+\d)(?=.*[#$%&!@])(?=*[a-z])(?=*[A-Z]).{8,}$
+
+const reglas = {
+    textos: /^[a-zA-ZÁ-ÿ\s]{1,40}$/, //SOLO LETRAS
+    numeros: /^\d{7,10}$/, //SOLO NÚMEROS
+    correo: /^[a-zA-Z0-9_+-.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, //CORREOS
+    password: /^(?=.+\d)(?=.*[#$%&!@])(?=.*[a-z])(?=.*[A-Z]).{8,}$/ //PASSWORD
+}
+//acceder al formulario
+let form = document.getElementById("frm-usuario")
+let campos = document.querySelectorAll("#frm-usuario input")
+
+//agregar listener de evento submit al formulario que se envia
+form.addEventListener('submit', e => {
+    e.preventDefault();//evitar que se envie el formulario para realizar las validaciones
+})
+
+const validarInput = (regla, input, grupo) => {
+    if (regla.test(input.value)) {
+        document.getElementById(`g-${grupo}`).classList.remove('error');
+        document.getElementById(`g-${grupo}`).classList.add('success');
+        document.querySelector(`#g-${grupo} i`).classList.add('fa-square-check');
+        document.querySelector(`#g-${grupo} i`).classList.remove('fa-circle-exclamation');
+        document.querySelector(`#g-${grupo} .msn-error`).classList.remove('msn-error-visible')
+    } else {
+        document.getElementById(`g-${grupo}`).classList.add('error');
+        document.getElementById(`g-${grupo}`).classList.remove('success');
+        document.querySelector(`#g-${grupo} i`).classList.remove('fa-square-check');
+        document.querySelector(`#g-${grupo} i`).classList.add('fa-circle-exclamation');
+        document.querySelector(`#g-${grupo} .msn-error`).classList.add('msn-error-visible')
+    }
+}
+
+const validarcampos = (e) => {
+    console.log("se genero un evento sobre el input" + e.target.name);
+    switch (e.target.name) {
+        case "numdoc":
+            validarInput(reglas.numeros,e.target ,e.target.name);
+            break;
+        case "nombre":
+            validarInput(reglas.textos,e.target ,e.target.name);
+            break;
+        case "apellido":
+            validarInput(reglas.textos,e.target,e.target.name);
+            break;
+        case "telefono":
+            validarInput(reglas.numeros,e.target ,e.target.name);
+            break;
+        case "correo":
+            validarInput(reglas.correo,e.target ,e.target.name);
+            break;
+        case "password":
+            validarInput(reglas.password,e.target ,e.target.name);
+            break;
+        default:
+            alert("no se ha recibido ningun evento");
+            break;
+    }
+}
+
+/* Aqui se hace uno por uno
+const validarcampos=(e)=>{
+    console.log("se genero un evento sobre el input"+e.target.name);
+    switch(e.target.name){
+        case "numdoc":
+            if (reglas.numeros.test(e.target.value)) {
+                document.getElementById('g-numdoc').classList.remove('error');
+                document.getElementById('g-numdoc').classList.add('success');
+                document.querySelector('#g-numdoc i').classList.add('fa-square-check');
+                document.querySelector('#g-numdoc i').classList.remove('fa-circle-exclamation');
+                document.querySelector('#g-numdoc .msn-error').classList.remove('msn-error-visible')
+            } else {
+                document.getElementById('g-numdoc').classList.add('error');
+                document.getElementById('g-numdoc').classList.remove('success');
+                document.querySelector('#g-numdoc i ').classList.remove('fa-square-check');
+                document.querySelector('#g-numdoc i ').classList.add('fa-circle-exclamation');
+                document.querySelector('#g-numdoc .msn-error').classList.add('msn-error-visible')
+            }
+        break;
+        case "nombre":
+
+        break;
+        case "apellido":
+
+        break;
+        case "telefono":
+
+        break;
+        case "correo":
+
+        break;
+        case "password":
+
+        break;
+        default:
+            alert("no se ha recibido ningun evento");
+        break;
+    }
+}
+*/
+campos.forEach((campo) => {
+    campo.addEventListener("keyup", validarcampos);
+    campo.addEventListener("blur", validarcampos);
+})
